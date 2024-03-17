8f8f2190234548c6fd6e12ee51b43c10db9bfcaa
karolkisinski <karol.kisinski@wp.pl>
2024-03-17 12:53:57 +0100

    updated urls list with AddMeasurement endpoint

--- src/measurements/urls.py
+++ src/measurements/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from measurements.views import HydroponicSystemMeasurementsListCreate, Last10MeasurementsList # noqa
+from measurements.views import HydroponicSystemMeasurementsListCreate, Last10MeasurementsList, AddMeasurement # noqa
 
 urlpatterns = [
     path('<int:system_id>/', HydroponicSystemMeasurementsListCreate.as_view(),
          name='system_measurements'),
     path('<int:system_id>/last10/', Last10MeasurementsList.as_view(),
          name='last_10_measurements'),
+    path('<int:system_id>/add/', AddMeasurement.as_view(), name='add_measurement'),
 ]
